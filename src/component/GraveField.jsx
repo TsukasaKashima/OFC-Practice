34bd92261e32ff764702308ab1183bdedfcaf742
azusa-mori <azusa_19911129@yahoo.co.jp>
2020-08-08 18:44:56 +0900

    #19 カードのドラッグアンドドロップを再度実装(Game.jsxのstateに依存させる形で実現)

---
+++ src/component/GraveField.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../App.css";
+import Card from "./Card";
+export default function GraveField(props) {
+  return (
+    <React.Fragment>
+      {[0, 1, 2, 3].map(index => {
+        return (
+          <Card
+            index={index}
+            fieldKey={props.fieldKey}
+            field={props.fieldCard}
+            fieldSetter={props.fieldSetter}
+          />
+        );
+      })}
+    </React.Fragment>
+  );
+}
--- src/component/SelfField.jsx
+++ src/component/SelfField.jsx
@@ -5,114 +5,40 @@ export default function SelfField(props) {
   return (
     <React.Fragment>
       <div className="box-row">
-        {props.fieldCard[0] !== undefined ? (
-          <Card
-            number={props.fieldCard[0].number}
-            type={props.fieldCard[0].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[1] !== undefined ? (
-          <Card
-            number={props.fieldCard[1].number}
-            type={props.fieldCard[1].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[2] !== undefined ? (
-          <Card
-            number={props.fieldCard[2].number}
-            type={props.fieldCard[2].type}
-          />
-        ) : (
-          <Card />
-        )}
+        {[0, 1, 2].map(index => {
+          return (
+            <Card
+              index={index}
+              fieldKey={props.fieldKey}
+              field={props.fieldCard}
+              fieldSetter={props.fieldSetter}
+            />
+          );
+        })}
       </div>
       <div className="box-row">
-        {props.fieldCard[3] !== undefined ? (
-          <Card
-            number={props.fieldCard[3].number}
-            type={props.fieldCard[3].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[4] !== undefined ? (
-          <Card
-            number={props.fieldCard[4].number}
-            type={props.fieldCard[4].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[5] !== undefined ? (
-          <Card
-            number={props.fieldCard[5].number}
-            type={props.fieldCard[5].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[6] !== undefined ? (
-          <Card
-            number={props.fieldCard[6].number}
-            type={props.fieldCard[6].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[7] !== undefined ? (
-          <Card
-            number={props.fieldCard[7].number}
-            type={props.fieldCard[7].type}
-          />
-        ) : (
-          <Card />
-        )}
+        {[3, 4, 5, 6, 7].map(index => {
+          return (
+            <Card
+              index={index}
+              fieldKey={props.fieldKey}
+              field={props.fieldCard}
+              fieldSetter={props.fieldSetter}
+            />
+          );
+        })}
       </div>
       <div className="box-row">
-        {props.fieldCard[8] !== undefined ? (
-          <Card
-            number={props.fieldCard[8].number}
-            type={props.fieldCard[8].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[9] !== undefined ? (
-          <Card
-            number={props.fieldCard[9].number}
-            type={props.fieldCard[9].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[10] !== undefined ? (
-          <Card
-            number={props.fieldCard[10].number}
-            type={props.fieldCard[10].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[11] !== undefined ? (
-          <Card
-            number={props.fieldCard[11].number}
-            type={props.fieldCard[11].type}
-          />
-        ) : (
-          <Card />
-        )}
-        {props.fieldCard[12] !== undefined ? (
-          <Card
-            number={props.fieldCard[12].number}
-            type={props.fieldCard[12].type}
-          />
-        ) : (
-          <Card />
-        )}
+        {[8, 9, 10, 11, 12].map(index => {
+          return (
+            <Card
+              index={index}
+              fieldKey={props.fieldKey}
+              field={props.fieldCard}
+              fieldSetter={props.fieldSetter}
+            />
+          );
+        })}
       </div>
     </React.Fragment>
   );
