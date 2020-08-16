882856ee1c110c867b43663af98f06d2144d5377
鹿島司 <kashimatsukasa@air.local>
2020-08-16 14:50:17 +0900

    feature/10

--- src/component/SelectBox.jsx
+++ src/component/SelectBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../App.css";
 import SelectBoxCard from "./SelectBoxCard";
 import { DropTarget } from "react-drag-drop-container";
